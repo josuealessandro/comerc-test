50f57309078366e08990823ee25c7d5cefc4dd84
Josué Alessandro <josuealessandro@gmail.com>
2023-11-23 13:38:17 -0300

    Ajustes na migrations de produto

--- app/database/migrations/2023_11_23_145721_create_products_table.php
+++ app/database/migrations/2023_11_23_145721_create_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->integer('price_cents');
             $table->timestamps();
